67825575a3af515e7a5ee745c333c356180bb349
Brian Martinez <100006298+buhraiyun@users.noreply.github.com>
2024-01-10 16:28:14 -0800

    Added 'Account' constructor method to 'DataSoure.java'; added '.vscode' directory to '.gitignore' .

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -50,17 +50,41 @@ public class DataSource {
     return customer;
   }
 
+  public static Account getAccount(int accountId) {
+    String sql = "select * from accounts where id = ?";
+    Account account = null;
+
+    try (Connection connection = connect();
+        PreparedStatement statement = connection.prepareStatement(sql)) {
+
+      statement.setInt(1, accountId);
+      try (ResultSet resultSet = statement.executeQuery()) {
+        account = new Account(
+            resultSet.getInt("id"),
+            resultSet.getString("type"),
+            resultSet.getDouble("balance"));
+
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+    return account;
+
+  }
+
   public static void main(String[] args) {
     // Test Cases:
-    /*
-     * Customer customer = getCustomer("twest8o@friendfeed.com");
-     * System.out.println(customer.getName());
-     * 
-     * Customer customer2 = getCustomer("sobu@squarespace.com");
-     * System.out.println(customer2.getName());
-     * 
-     * Customer customer3 = getCustomer("sfealty2e@cnbc.com");
-     * System.out.println(customer3.getName());
-     */
+
+    Customer customer = getCustomer("twest8o@friendfeed.com");
+    Account account = getAccount(customer.getAccountId());
+    System.out.println(account.getBalance());
+
+    // Customer customer2 = getCustomer("sobu@squarespace.com");
+    // System.out.println(customer2.getName());
+
+    // Customer customer3 = getCustomer("sfealty2e@cnbc.com");
+    // System.out.println(customer3.getName());
+
   }
 }
